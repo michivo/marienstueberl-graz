b0d50bb424a64e167910bfdc167a28d0f6a0d274
Michael Faschinger <mfaschinger@protossecurity.com>
2025-02-03 20:33:28 +0100

    turn page off

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,5 @@
-<script lang="ts">
-	import { currentUser } from '../stores/currentUser';
-	import LoggedInHeader from '../components/headers/LoggedInHeader.svelte';
-	import EmptyHeader from '../components/headers/EmptyHeader.svelte';
-	import LoadingHeader from '../components/headers/LoadingHeader.svelte';
-	import { onMount } from 'svelte';
-	import { page } from '$app/stores';
-</script>
-
 <div class="container">
-	{#if $currentUser.state === 'loggedIn'}
-		<LoggedInHeader />
-	{:else if $currentUser.state === 'loggedOut'}
-		<EmptyHeader />
-	{:else}
-		<LoadingHeader />
-	{/if}
 	<slot></slot>
-	<footer>
-		<a href="/imprint">Impressum</a>
-	</footer>
 </div>
 
 <style>
@@ -26,17 +7,5 @@
 		min-height: 100dvh;
 		display: flex;
 		flex-direction: column;
-	}
-
-	footer {
-		border-top: 1px solid #ccc;
-		display: flex;
-		justify-content: center;
-		padding: 0.25rem;
-
-		a {
-			text-transform: uppercase;
-			color: var(--secondary-dark);
-		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,77 +1,24 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
-	import { authProviders, firebaseAuth } from '../services/firebase';
-	import { browser } from '$app/environment';
-	import { currentUser } from '../stores/currentUser';
-	import { goto } from '$app/navigation';
-	import Spinner from '../components/misc/Spinner.svelte';
-	import { getIdTokenResult } from 'firebase/auth';
-
-	let firebaseUi = undefined as undefined | any;
-
-	onMount(async () => {
-		if ($currentUser.state === 'loggedOut') {
-			const firebaseUi = await firebaseAuthUi();
-			if (firebaseUi) {
-				firebaseUi.start('#firebaseui-auth-container', {
-					signInOptions: authProviders
-				});
-			}
-		}
-	});
-
-	currentUser.subscribe(async (value) => {
-		if (value.state === 'loggedIn' && value.user) {
-			const idToken = await getIdTokenResult(value.user);
-			if (idToken.claims['admin']) {
-				goto('/admin/clients');
-			} else if (idToken.claims['privilegedUser']) {
-				goto('/handout');
-			} else {
-				goto('/client');
-			}
-		}
-		else if(value.state === 'loggedOut') {
-			const firebaseUi = await firebaseAuthUi();
-			if (firebaseUi) {
-				firebaseUi.start('#firebaseui-auth-container', {
-					signInOptions: authProviders
-				});
-			}
-		}
-	});
-
-	async function firebaseAuthUi() {
-		if (browser && !firebaseUi) {
-			const firebaseui = await import('firebaseui');
-			firebaseUi = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebaseAuth);
-		}
-		return firebaseUi;
-	}
+<script>
+	import GoneIcon from "../components/icons/GoneIcon.svelte";
 </script>
 
-<div class="landing-content">
-	{#if $currentUser.state === 'pending'}
-		<Spinner />
-	{:else if $currentUser.state === 'loggedOut'}
-		<div id="firebaseui-auth-container"></div>
-	{/if}
+<div class="broken-content">
+	<GoneIcon />
+	<h1>Diese Seite wurde mangels Interesse eingestellt</h1>
 </div>
 
 <style lang="scss">
-	@import '/css/firebase-ui.css';
-
-	.landing-content {
+	.broken-content {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
 		align-items: center;
+		justify-content: center;
 		height: 100vh;
-		gap: 2rem;
+	}
 
-		div {
-			width: 30rem;
-			max-width: 100%;
-		}
+	h1 {
+		font-size: 5rem;
+		font-weight: 100;
+		text-align: center;
 	}
 </style>
